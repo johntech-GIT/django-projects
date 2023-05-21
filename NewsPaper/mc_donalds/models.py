93b36b8f1a848950ecfaef5509f587afe6bdb131
johntech-GIT <tea-expansion@yandex.ru>
2023-05-21 19:24:39 +0300

    21_05_2023

--- NewsPaper/mc_donalds/models.py
+++ NewsPaper/mc_donalds/models.py
@@ -1,7 +1,5 @@
+from datetime import *
 from django.db import models
-
-
-# from datetime import datetime
 
 
 class Product(models.Model):
@@ -43,15 +41,33 @@ class Order(models.Model):
     pickup = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product", through='ProductOrder')
 
-    # def get_duration(self):
-    #     if self.complete:  # если завершён, возвращаем разность объектов
-    #         return (self.time_out - self.time_in).total_seconds() // 60
-    #     else:  # если ещё нет, то сколько длится выполнение
-    #         return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds()
+        else:
+            return (datetime.now() - self.time_in).total_seconds()
+
+
 
 
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     _amount = models.IntegerField(default=1, db_column='amount')
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self._amount
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+    age = models.IntegerField(blank=True)
+    email = models.CharField()
--- main.py
+++
@@ -1,4 +0,0 @@
-product_1 = Product(name = "Витая пара (3 м)", price = 993.0)
-product_1.save()
-
-product_2 = Product.objects.create(name = "Клавиатура", price = 1060.0)
